6f656d7fd8f68831dfb877dc71a5892f3bed54d3
Erick Celio <erickcelio@hotmail.com>
2021-02-06 18:27:48 -0300

    ensure LoginPage call method auth on form submit

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -65,7 +65,8 @@ class LoginPage extends StatelessWidget {
                         stream: presenter.isFormValidStream,
                         builder: (context, snapshot) {
                           return RaisedButton(
-                            onPressed: snapshot.data == true ? () {} : null,
+                            onPressed:
+                                snapshot.data == true ? presenter.auth : null,
                             child: Text('Entrar'.toUpperCase()),
                           );
                         }),
