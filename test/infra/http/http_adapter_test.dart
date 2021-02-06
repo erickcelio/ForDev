9470db17c407d668370124b3e451b7b5b0f2096e
Erick Celio <erickcelio@hotmail.com>
2021-02-06 11:12:29 -0300

    ensure http adapter return data if post returns 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -6,12 +6,14 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-class HttpAdapter {
+import 'package:for_dev/data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request(
+  Future<Map> request(
       {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
@@ -19,7 +21,9 @@ class HttpAdapter {
     };
 
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(url, headers: headers, body: jsonBody);
+    final response = await client.post(url, headers: headers, body: jsonBody);
+
+    return jsonDecode(response.body);
   }
 }
 
@@ -38,6 +42,10 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
+      when(client.post(any,
+              headers: anyNamed('headers'), body: anyNamed('body')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
@@ -50,9 +58,21 @@ void main() {
     });
 
     test('Should call post without body', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
       await sut.request(url: url, method: 'post');
 
       verify(client.post(any, headers: anyNamed('headers')));
+    });
+
+    test('Should return data if post returns 200', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, {'any_key': 'any_value'});
     });
   });
 }
