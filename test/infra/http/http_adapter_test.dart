4213462666ebc644c114b176491c8da764653766
Erick Celio <erickcelio@hotmail.com>
2021-02-06 11:47:59 -0300

    ensure HttpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -37,6 +37,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -130,6 +134,14 @@ void main() {
 
     test('Should ServerError if post returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
