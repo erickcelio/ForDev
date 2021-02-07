54a542a93343224730acf5a5a24224b3cc335292
Erick Celio <erickcelio@hotmail.com>
2021-02-07 20:29:07 -0300

    ensure ValidationComposite return first error found

--- test/validation/validators/validation_compose_test.dart
+++ test/validation/validators/validation_compose_test.dart
@@ -14,7 +14,16 @@ class ValidationComposite implements Validation {
     @required String field,
     @required String value,
   }) {
-    return null;
+    String error;
+    for (final validation in validations) {
+      error = validation.validate(value);
+
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+
+    return error;
   }
 }
 
@@ -57,5 +66,15 @@ void main() {
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
+  });
+
+  test('Should return first error', () {
+    mockValidation1('error_1');
+    mockValidation2('error_2');
+    mockValidation3('error_3');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error_1');
   });
 }
