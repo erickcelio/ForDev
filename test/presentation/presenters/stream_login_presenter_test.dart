74c70c55eb18342a6b3bcff1e567a56b9d39cd94
Erick Celio <erickcelio@hotmail.com>
2021-02-07 18:22:09 -0300

    ensure StreamLoginPresenter emits form valid event if form is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -98,4 +98,16 @@ void main() {
     sut.validateEmail(password);
     sut.validatePassword(password);
   });
+
+  test('Should emit isFormValid false if any field is invalid', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.passwordErrorStream
+        .listen(expectAsync1((error) => expect(error, null)));
+
+    expectLater(sut.isFormValidStream, emitsInOrder([false, true]));
+
+    sut.validateEmail(email);
+    await Future.delayed(Duration.zero);
+    sut.validatePassword(password);
+  });
 }
